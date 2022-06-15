7755c4a711e33d7748a1a3e9c452ed7caaf04658
acorson <balonee94@gmail.com>
2022-06-15 15:07:38 +0100

    some stuff

---
+++ star trek/DataContext.py
@@ -0,0 +1,53 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+            specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def select_all(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+def select_all_from_season(conn, season):
+    cur = conn.cursor
+    cur.execute("SELECT * FROM list WHERE season=?", (season,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+def main():
+    database = r""
+
+    conn = create_connection(database)
+    with conn:
+        print("query by season")
+        select_all_from_season(conn, int(input("what season do you want a list of?")))
+
+        print("query all")
+        select_all(conn)
