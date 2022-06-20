e07b0437490ac40d3b4ba0c7d71921b0308ba117
acorson <balonee94@gmail.com>
2022-06-20 21:00:15 +0100

    populating DB and cleaning functions

--- star trek/main.py
+++ star trek/main.py
@@ -1,4 +1,14 @@
-import DataContext
+import DataContext as Dc
 
 print("welcome to this application")
-DataContext.main()
+
+decide = int(input("what are you looking for 1.everything 2.Specific season"))
+
+if decide == 1:
+    Dc.main()
+
+elif decide == 2:
+    Dc.main()
+
+else:
+    print("you have not entered a valid response to the question")
